ccf5e097ce32e035d7186020ae2cf7f0b23cdf6c
ձեÖErr0r07 <enfendav@gmail.com>
2026-06-05 14:00:07 +0000

    Correct pins from official LCDWIKI docs

--- include/User_Setup.h
+++ include/User_Setup.h
@@ -1,11 +1,15 @@
 #define ILI9341_DRIVER
 #define TFT_WIDTH  240
 #define TFT_HEIGHT 320
-#define TFT_CS   10
-#define TFT_DC   46
+#define TFT_MISO 13
 #define TFT_MOSI 11
 #define TFT_SCLK 12
+#define TFT_CS   10
+#define TFT_DC   46
+#define TFT_RST  -1
 #define TFT_BL   45
 #define TFT_BACKLIGHT_ON HIGH
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
 #define SPI_FREQUENCY 40000000
-#define SUPPORT_TRANSACTIONS
